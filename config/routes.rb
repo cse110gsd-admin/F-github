5cc4dec0ec9c1be9913aa43243b906a8eb11211d
Will <wjdhollow@gmail.com>
2011-05-25 03:56:07 -0700

    Changed how routes are configured, hid password field in User class

--- config/routes.rb
+++ config/routes.rb
@@ -1,90 +1,28 @@
-Xlogger::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  resources :workouts
-    resources :events
+Xlogger::Application.routes.draw do |map|
+  # map.connect ':controller/:action/:id'
+  # params = { :controller => 'blog',
+  #            :action => 'edit', 
+  #            :id    =>  '22'
+  #            }
+
+  # sets up a default map
+  #map.connect ':controller/:action/:id', :controller => 'pages'
   resources :warmups
-  resources :templates
+  resource  :templates
   resources :exercises
   resources :entries
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
 
+  map.signup 'signup', :controller => 'users', :action => 'new'
+  map.signin 'signin', :controller => 'sessions', :action => 'new'
+  map.signout 'signout', :controller => 'sessions', :action => 'destroy'
 
-  get "calendar/index"
+  map.contact 'contact', :controller => 'pages', :action => 'contact'
+  map.about 'about', :controller => 'pages', :action => 'about'
+  map.help 'help', :controller => 'pages', :action => 'help'
 
+  map.calendar 'calendar', :controller => 'calendar', :action => 'index'
 
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
-
-  match '/signup', :to => 'users#new'
-  match '/signin', :to => 'sessions#new'
-  match '/signout', :to => 'sessions#destroy'
-
-  match '/contact', :to => 'pages#contact'
-  match '/about',   :to => 'pages#about'
-  match '/help',    :to => 'pages#help'
-
-  match 'calendar', :to => "calendar#index"
-
-
-  root :to => 'pages#home' 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
-end
+  map.root :controller => 'pages', :action => 'home'
+end  
