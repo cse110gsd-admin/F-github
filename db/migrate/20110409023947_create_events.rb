de1964abed0d25c67fd54960390661587033cb3d
Jake Rabinowitz <jrabinow@ucsd.edu>
2011-05-25 16:31:36 -0700

    events.user_id added to db

--- db/migrate/20110409023947_create_events.rb
+++ db/migrate/20110409023947_create_events.rb
@@ -1,6 +1,7 @@
 class CreateEvents < ActiveRecord::Migration
   def self.up
     create_table :events do |t|
+      t.integer :user_id
       t.string :title
       t.datetime :starts_at
       t.datetime :ends_at
