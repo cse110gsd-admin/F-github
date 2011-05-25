eecdb119b60c20d59b2b6eaf1719596d3e409314
Jake Rabinowitz <jrabinow@ucsd.edu>
2011-05-25 16:46:57 -0700

    changetable user_id column

--- db/migrate/20110409023947_create_events.rb
+++ db/migrate/20110409023947_create_events.rb
@@ -1,16 +1,19 @@
 class CreateEvents < ActiveRecord::Migration
   def self.up
     create_table :events do |t|
-      t.integer :user_id
       t.string :title
       t.datetime :starts_at
       t.datetime :ends_at
       t.boolean :all_day
       t.text :description
      # t.integer :weight
-
       t.timestamps
     end
+
+    change_table :events do |t|
+      t.add_column, :user_id, :integer
+    end
+
   end
 
   def self.down
